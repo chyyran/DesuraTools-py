04fc5bd863d210c1ec562ab89c893956426b6d3c
Ronny Chan <ronny6993@gmail.com>
2013-11-03 20:37:18 -0500

    Add Steam Support

--- steammanager.py
+++ steammanager.py
@@ -1,15 +1,30 @@
 #coding=utf-8
 __author__ = 'ron975'
-"""
-This file is part of DesuraTools
-"""
-import shortcutmanager
-
-manager = shortcutmanager.SteamShortcutManager("D:\Steam\userdata\81548059\config\shortcuts.vdf")
+import os
+import steam.steam_shortcut_manager as shortcutmanager
+import steam.steam_user_manager as usermanager
 
 
-def insert_shortcut(name, exe):
+def insert_shortcut(manager, name, exe, icon=""):
     for shortcut in manager.shortcuts:
         if shortcut.appname == name:
-            pass
-    manager.add_shortcut(name, exe, os.path.dirname(exe))
+            print shortcut.appname, "already exists, skipping"
+            return
+    manager.add_shortcut(name, "\""+exe+"\"", "\""+os.path.dirname(exe)+"\"", icon=icon)
+    print "Added game", name, "to the Steam Library"
+
+def associate_ids_with_users():
+    steamusers = []
+    for id in usermanager.user_ids_on_this_machine():
+        steamusers.append({'steamid32': id, 'customurl': usermanager.name_from_communityid32(id)})
+    return steamusers
+
+def choose_userdata_folder():
+    for index, user in enumerate(associate_ids_with_users()):
+        print index+1, user['steamid32'], user['customurl']
+
+    index = int(raw_input())
+    return shortcutmanager.SteamShortcutManager(
+        usermanager.shortcuts_file_for_user_id(associate_ids_with_users()[index-1]['steamid32'])
+    )
+
--- testui/run.py
+++ testui/run.py
@@ -1,12 +1,30 @@
 #coding=utf-8
 __author__ = 'ron975'
 import webbrowser
-
+from PIL import Image
 from generatehtml import TestReport
 
-print "What is your Desura username?"
-username = raw_input()
-try:
-    webbrowser.open(str(TestReport(username)))
-except Exception, e:
-    print "Invalid Desura Username", e.message, e.args
+import installedgames, steammanager
+
+print "Options: report, steam"
+options = raw_input()
+if options == "report":
+    print "What is your Desura username?"
+    username = raw_input()
+    try:
+        print "Generating report"
+        webbrowser.open(str(TestReport(username)))
+    except Exception, e:
+        print "Invalid Desura Username", e.message, e.args
+if options == "steam":
+    print "Adding games to steam"
+    print "Which is your Steam Account?"
+    manager = steammanager.choose_userdata_folder()
+    for game in installedgames.get_games():
+        if game.icon.lower().endswith("jpeg") or game.icon.lower().endswith("jpg"):
+            Image.open(game.icon).save(game.icon+".png")
+            game.icon += ".png"
+        steammanager.insert_shortcut(manager, game.name, game.exe, icon=game.icon)
+    manager.save()
+    for shortcut in manager.shortcuts:
+        print shortcut.appname
